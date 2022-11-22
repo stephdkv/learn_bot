9c12dbe5c5fab4c639a80e99019c185a3d20d0d1
Stepan <freenlol@gmail.com>
2022-11-22 10:27:37 +0600

    Смайлики и картинки2

--- bot.py
+++ bot.py
@@ -1,23 +1,64 @@
 import logging
 import settings
+from glob import glob
+from emoji import emojize
+from random import randint, choice
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def greet_user(update, context):
     print('Вызван /start')
-    update.message.reply_text('Здравствуй, пользователь!')
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f"Здравствуй, пользователь!{context.user_data['emoji']}")
 
 def talk_to_me(update, context):
+    context.user_data['emoji'] = get_smile(context.user_data)
     text = update.message.text
     print(text)
-    update.message.reply_text(text)
+    update.message.reply_text(f"{text} {context.user_data['emoji'] }")
+
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile)
+    return user_data['emoji']
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_number(user_number)
+        except (TypeError, ValueError):
+            message = 'Введите целое число'
+    else:
+        message = 'Введите число'
+    update.message.reply_text(message)
+
+def play_random_number(user_number):
+    bot_number = randint(user_number-10, user_number+10)
+    if user_number > bot_number:
+        message = f'Ваше число {user_number}, мое {bot_number}. Вы победили!'
+    elif user_number == bot_number:
+        message = f'Ваше число {user_number}, мое {bot_number}. Ничья!'
+    else:
+        message = f'Ваше число {user_number}, мое {bot_number}. Вы проиграли!'
+    return message
+
+def send_cat_pictures(update, context):
+    cat_photo_list = glob('images/cat*.jp*g')
+    cat_photo_filename = choice(cat_photo_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id = chat_id, photo = open(cat_photo_filename, 'rb') )
 
 def main():
     mybot = Updater(settings.API_KEY, use_context = True,)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(CommandHandler('cat', send_cat_pictures))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
